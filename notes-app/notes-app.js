4e5fc68852247dcb799198019be2db7bb5cdf75f
MichaelWiss <michael.wiss@gmail.com>
2018-07-10 12:14:25 -0500

    todo

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,4 +1,13 @@
-
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to China'
+}, {
+	title: 'habbits to work on',
+	body: 'more reading.'
+}, {
+	title: 'Office',
+	body: 'vacum'
+}]
 // const el = document.querySelector('p');
 // el.remove();
 
@@ -7,3 +16,7 @@ const ps = document.querySelectorAll('p')
 ps.forEach(function (p) {
    p.textContent = '*******'
 })
+
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from Javascript'
+document.querySelector('body').appendChild(newParagraph)

41ec11684963eb4db118f863dbea5a2739c42415
MichaelWiss <michael.wiss@gmail.com>
2018-07-13 15:47:41 -0500

    dropdown filter

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -65,8 +65,12 @@ document.querySelector('#search-text').addEventListener('input', function(e) {
 // 	e.target.elements.firstName.value = ''
 // })
 
-document.querySelector('#for-fun').addEventListener('change', function(e) {
-	console.log(e.target.checked)
+// document.querySelector('#for-fun').addEventListener('change', function(e) {
+// 	console.log(e.target.checked)
+// })
+
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+	
 })
 
 

4fb599d75a1c574ac0686b94ca9775c2edcb77d5
MichaelWiss <michael.wiss@gmail.com>
2018-07-14 11:51:51 -0500

    localStorage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -25,6 +25,25 @@ const filters = {
 	searchText: ''
 }
 
+//localStorage.setItem('location', 'New York')
+// console.log(localStorage.getItem('location'))
+
+// localStorage.removeItem('location')
+
+// localStorage.clear
+
+// const user = {
+// 	name: 'Michael',
+// 	age: 36
+// }
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
+
 const renderNotes = function (notes, filters) {
 	const filteredNotes = notes.filter(function (note) {
 		return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
@@ -70,7 +89,7 @@ document.querySelector('#search-text').addEventListener('input', function(e) {
 // })
 
 document.querySelector('#filter-by').addEventListener('change', function (e) {
-	
+	console.log(e.target.value)
 })
 
 

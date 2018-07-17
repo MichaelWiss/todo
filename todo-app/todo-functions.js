a1c4f4091db9727731d0cca0592c0e2b06ba4d89
MichaelWiss <michael.wiss@gmail.com>
2018-07-17 12:11:05 -0500

    remove button

--- todo-app/todo-functions.js
+++ todo-app/todo-functions.js
@@ -13,6 +13,15 @@ const savedTodos = function (todos) {
 	localStorage.setItem('todos', JSON.stringify(todos))
 }
 
+const removeTodo = function (id) {
+	const todoIndex = todos.findIndex(function (todo) {
+		return todo.id === id
+	})
+	if (todoIndex > -1) {
+		todos.splice(todoIndex, 1)
+	}
+}
+
 
 
 const renderTodos = function (todos, filters) {
@@ -64,6 +73,11 @@ const generateTodoDOM = function (todo) {
     // Setup the remove button
     removeButton.textContent = 'x'
     todoEl.appendChild(removeButton)
+    removeButton.addEventListener('click', function () {
+    	removeTodo(todo.id)
+    	savedTodos(todos)
+    	renderTodos(todos, filters)
+    })
 
 
 	return todoEl

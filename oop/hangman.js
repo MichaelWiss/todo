afd3b1bf2d655bf1880983134f0e78d89db10ce4
MichaelWiss <michael.wiss@gmail.com>
2018-07-25 10:54:48 -0500

    +=

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,6 +1,21 @@
 const Hangman = function (word, remainingGuesses) {
 	this.word = word.toLowerCase().split('')
 	this.remainingGuesses = remainingGuesses
+	this.guessedLetters = ['c']
+}
+
+Hangman.prototype.getPuzzle = function () {
+	let puzzle = ''
+    
+    this.word.forEach(letter) {
+    	if (this.guessedLetters.includes(letter) || letter === ' ') {
+          puzzle += letter
+    	} else{
+           puzzle += '*'
+    	}
+    }
+
+	return puzzle
 }
 
 

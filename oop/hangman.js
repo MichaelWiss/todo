fabdb64e4c0f752b09eaf241204e5cf65c6f7531
MichaelWiss <michael.wiss@gmail.com>
2018-07-25 10:37:13 -0500

    split

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,4 +1,12 @@
 const Hangman = function (word, remainingGuesses) {
-	this.word = word
+	this.word = word.toLowerCase().split('')
 	this.remainingGuesses = remainingGuesses
 }
+
+
+
+const game1 = new Hangman('cat' 2)
+console.log(game1)
+
+const game2 = new Hangman('New Jersey', 4)
+console.log(game2)

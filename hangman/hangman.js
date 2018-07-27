b9c6e8b3b3ab4b3dc50f667dba63d074863c633d
MichaelWiss <michael.wiss@gmail.com>
2018-07-27 12:41:04 -0500

    classes

--- hangman/hangman.js
+++ hangman/hangman.js
@@ -6,8 +6,38 @@ class Hangman {
 	 this.status = 'playing'
 	}
 	calculateStatus() {
-		
+		const finished = this.word.every((letter) => this.guessedLetters.includes(letter))
+   
+    if (this.remainingGuesses === 0) {
+    	this.status = 'failed'
+    } else if (finished) {
+    	this.status = 'finished'
+    } else {
+    	this.status = 'playing'
+    }
+}
+getStatusMessage() {
+	if (this.status === 'playing') {
+		return `Guesses left: ${this.remainingGuesses}`
+	} else if (this.status === 'failed') {
+		return `Nice try! The word was "${this.word.join('')}".` 
+	} else {
+		return  `Great work! You guessed the word!`
 	}
+  }
+  getPuzzle() {
+  	let puzzle = ''
+    
+    this.word.forEach((letter) => {
+    	if (this.guessedLetters.includes(letter) || letter === ' ') {
+          puzzle += letter
+    	} else{
+           puzzle += '*'
+    	}
+    })
+
+	return puzzle
+  }
 }
 
 

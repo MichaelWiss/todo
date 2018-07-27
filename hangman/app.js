70c19e33bf6da0c7f27783dfccd72b706f41d4ea
MichaelWiss <michael.wiss@gmail.com>
2018-07-27 14:48:53 -0500

    http request

--- hangman/app.js
+++ hangman/app.js
@@ -12,6 +12,16 @@ window.addEventListener('keypress', function (e) {
 	game1.makeGuess(guess)
 	puzzleEl.textContent = game1.puzzle
     guessesEl.textContent = game1.statusMessage
-    
-
 })
+
+const request = new XMLHttpRequest() 
+
+request.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState ===4) {
+		const data = JSON.parse(e.target.responseText)
+		console.log(data)
+	}
+})
+
+request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.send()

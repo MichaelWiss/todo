c8bfe7024e0b56a6df7e32b637bf690cd8685081
MichaelWiss <michael.wiss@gmail.com>
2018-07-28 11:52:59 -0500

    request.find

--- hangman/app.js
+++ hangman/app.js
@@ -17,12 +17,27 @@ window.addEventListener('keypress', (e) => {
 const request = new XMLHttpRequest() 
 
 request.addEventListener('readystatechange', (e) => {
-	if (e.target.readyState ===4) {
-		console.log(e.target.status)
+	if (e.target.readyState === 4 && e.target.status === 200) {
 		const data = JSON.parse(e.target.responseText)
 		console.log(data)
+	} else if (e.target.readyState === 4) {
+		console.log('An error has taken place')
 	}
 })
 
-request.open('GET', 'http://puzzle.mead.io/puzzle')
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
+
+const countryCode = "US"
+const requestCountry = new XMLHttpRequest()
+
+requestCountry.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState === 4 && e.target.status === 200) {
+		const data = JSON.parse(e.target.responseText)
+		const country = data.find((country) => country.alpha2Code === countryCode)
+		console.log(country.name)
+	}
+})
+
+requestCountry.open('GET', 'http://restcountries.eu/rest/v2/all')
+requestCountry.send()

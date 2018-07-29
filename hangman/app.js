8674e2292d5a06912677338aca84eb6438aad2d0
MichaelWiss <michael.wiss@gmail.com>
2018-07-29 18:07:12 -0500

    asn async

--- hangman/app.js
+++ hangman/app.js
@@ -40,16 +40,4 @@ getPuzzle((error, puzzle) => {
 // request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 // request.send()
 
-// const countryCode = "US"
-// const requestCountry = new XMLHttpRequest()
 
-// requestCountry.addEventListener('readystatechange', (e) => {
-// 	if (e.target.readyState === 4 && e.target.status === 200) {
-// 		const data = JSON.parse(e.target.responseText)
-// 		const country = data.find((country) => country.alpha2Code === countryCode)
-// 		console.log(country.name)
-// 	}
-// })
-
-// requestCountry.open('GET', 'http://restcountries.eu/rest/v2/all')
-// requestCountry.send()
--- hangman/requests.js
+++ hangman/requests.js
@@ -14,3 +14,20 @@ request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
 }
 
+const getCountry = () => {
+	const countryCode = "US"
+    const requestCountry = new XMLHttpRequest()
+
+    requestCountry.addEventListener('readystatechange', (e) => {
+	if (e.target.readyState === 4 && e.target.status === 200) {
+		const data = JSON.parse(e.target.responseText)
+		const country = data.find((country) => country.alpha2Code === countryCode)
+		console.log(country.name)
+	} else if (e.target.readyState ===4) {
+		console.log('Unable to fetch data')
+	}
+})
+
+requestCountry.open('GET', 'http://restcountries.eu/rest/v2/all')
+requestCountry.send()
+}

ebef8cc66526dd553b000a1f0bbc05f11de89e94
MichaelWiss <michael.wiss@gmail.com>
2018-07-30 16:49:58 -0500

    promise 2

--- hangman/requests.js
+++ hangman/requests.js
@@ -14,19 +14,21 @@ request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
 request.send()
 })
 
-const getCountry = (countryCode, callback) => {
-    const requestCountry = new XMLHttpRequest()
+const getCountry = (countryCod) => new Promise((resolve, reject) => {
+	const requestCountry = new XMLHttpRequest()
 
     requestCountry.addEventListener('readystatechange', (e) => {
 	if (e.target.readyState === 4 && e.target.status === 200) {
 		const data = JSON.parse(e.target.responseText)
 		const country = data.find((country) => country.alpha2Code === countryCode)
-		callback(undefined, country)
+		resolve(data.country)
 	} else if (e.target.readyState ===4) {
-		callback('Unable to fetch data')
+		reject('Unable to fetch data')
 	}
 })
 
 requestCountry.open('GET', 'http://restcountries.eu/rest/v2/all')
 requestCountry.send()
-}
+
+}) 
+    

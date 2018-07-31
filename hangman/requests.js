49600e1051c46695e860326a7ef276a6ffa62b09
MichaelWiss <michael.wiss@gmail.com>
2018-07-31 18:23:48 -0500

    errors

--- hangman/requests.js
+++ hangman/requests.js
@@ -20,27 +20,9 @@ const getCountry = (countryCode) => {
 			throw new Error('Unable to fetch data ')
 		}
 	}).then((data) => {
-           return country = data.find((country) => country.alpha2Code === countryCode)
-	})
+		return country = data.find((country) => country.alpha2Code === countryCode)
+})
 }
 
-//
 
-const getCountryOld = (countryCode) => new Promise((resolve, reject) => {
-	const requestCountry = new XMLHttpRequest()
-
-    requestCountry.addEventListener('readystatechange', (e) => {
-	if (e.target.readyState === 4 && e.target.status === 200) {
-		const data = JSON.parse(e.target.responseText)
-		const country = data.find((country) => country.alpha2Code === countryCode)
-		resolve(country)
-	} else if (e.target.readyState ===4) {
-		reject('Unable to fetch data')
-	}
-})
-
-requestCountry.open('GET', 'http://restcountries.eu/rest/v2/all')
-requestCountry.send()
-
-}) 
     

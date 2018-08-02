5147f442324a7bdd6223dc351307df8250837d3f
MichaelWiss <michael.wiss@gmail.com>
2018-08-02 14:43:02 -0500

    async/await

--- hangman/requests.js
+++ hangman/requests.js
@@ -15,14 +15,11 @@ const getCountry = async (countryCode) => {
 	const countryResponse = await fetch('http://restcountries.eu/rest/v2/all').then((response) => {
 		if (response.status === 200) {
 			const countryData = await response.json()
-           return countryData.json()
+           	return data.find((country) => country.alpha2Code === countryCode)
 		} else {
-			throw new Error('Unable to fetch data ')
+			throw new Error('Unable to fetch the country ')
 		}
-	}).then((countryData) => {
-		return country = data.find((country) => country.alpha2Code === countryCode)
-  })
-}
+}		
 
 const getLocation = () => {
 	return fetch('http://ipinfo.io/json?token=a0ee1b5b3dc541').then((response) => {
